da4d1d087d0f9fdcc6d22fc0c98baa58e9e68e11
Joel Wiesner <j.wiesner88@gmail.com>
2024-03-05 15:24:58 -0800

    Create characters.js

---
+++ characters.js
@@ -0,0 +1,105 @@
+ export const characters = [
+    {
+        name: "Eleven",        
+        image: "https://upload.wikimedia.org/wikipedia/en/5/52/Eleven_%28Stranger_Things%29.jpg",
+        blurb: "You embody a remarkable blend of bravery, optimism, and innocence, navigating a world fraught with supernatural dangers with unwavering determination. Your naive perspective on life adds a poignant depth to your character, as you learn to navigate the complexities of friendship, love, and sacrifice in a world you're only beginning to understand."
+    },
+    {
+        name: "Max Mayfield",
+        image: "https://static.wikia.nocookie.net/strangerthings8338/images/7/73/Max_Mayfield_-_S3.png",
+        blurb: "You bring a spirited and rebellious energy to the group, curious with a fierce independence. Despite the dangers lurking around every corner, your resilient spirit and loyalty to your friends shine through, as you learn to trust in others and embrace the true meaning of courage amidst the chaos."
+    },
+    {
+        name: "Will Byers",
+        image: "https://static.wikia.nocookie.net/strangerthings8338/images/b/bd/Will_Byers_S2.jpg",
+        blurb:  "You are marked by a cautious skepticism, grappling with the uncertainties of a world. Despite your doubts, your quiet courage and determination excel as you confront the unknown. You face the truth with an uncertain yet resilient spirit."
+    },
+    {
+        name: "Dustin Henderson",
+        image: "https://static.wikia.nocookie.net/strangerthings8338/images/7/7b/Dustin_S3.png",
+        blurb: "You have endearing wit and unwavering loyalty. You encapsulate the very spirit of resilience and companionship amidst the daunting trials of the supernatural realm. Intellect, humor, and heart make you an indispensable pillar of strength within a mysterious and puzzling world."
+    },
+    {
+        name: "Mike Wheeler",
+        image: "https://static.independent.co.uk/2021/04/21/20/newFile-4.jpg",
+        blurb: "You are a beacon of determination and compassion, not afriad to navigate through the turmoils of life with a steadfast heart and unwavering resolve. Your leadership, empathy, and loyalty illuminate the path through the darkest shadows, revealing that in the bonds of friendship lies the strength to conquer any challenge."
+    },
+    {
+        name: "Lucas Sinclair",
+        image: "https://compote.slate.com/images/122ef31a-9e5e-4abc-a2cd-a6aae2c75b5b.jpeg?crop=2880%2C1920%2Cx0%2Cy0&width=1280",
+        blurb: "In the labyrinth of the Upside Down, you stand as a sentinel of resilience and valor, with unwavering determination cutting through the fog of uncertainty. You're skeptical yet truehearted, proving that amidst the chaos, you're steadfast spirit is a beacon of hope and strength for your fellow companions."
+    },
+    {
+        name: "Jonathan Byers",
+        image: "https://images.immediate.co.uk/production/volatile/sites/3/2017/10/Stranger-Things-2-Jonathan-4f8c946.jpg?quality=90&resize=980,654",
+        blurb: "You're a soulful wanderer amidst the mysteries of the world, wielding a camera as both shield and lens into the enigmatic depths of the unknown. With quiet strength and profound empathy, you navigate a tangled web of secrets, proving that in the pursuit of truth and connection, your unyielding essence is a guiding light through the shadows of uncertainty."
+    },
+    {
+        name: "Nancy Wheeler",
+        image: "https://static.wikia.nocookie.net/strangerthings8338/images/7/7f/NancyWheeler.png",
+        blurb: "You're a fiery spirit ablazed with determination, taking fearless strides through the twisted tapestry of the unknown. Armed with reason and compassion, you unearth the buried truths of both the mundane and the extraordinary, demonstrating your unwavering courage and igniting hope and resilience in the darkest of realms."
+
+    },
+    {
+        name: "Steve Harrington",
+        image: "https://itswynnesworld.files.wordpress.com/2022/07/steve-harrington-with-bat-social-041718-1024x538-1.png?w=1024",
+        blurb: "You are an unexpected hero! You cruise with a blend of charm and courage that belies your initial facade. Through trials and transformations, you'll evolve from mere high school heartthrob to a stalwart defender! Proving that in the crucible of adversity, your growth and loyalty shine brightly, illuminating the path for those around you."
+    },
+    {
+        name: "Jim Hopper",
+        image: "https://hips.hearstapps.com/hmg-prod/images/hopper-stranger-things-3-1562101073.jpg",
+        blurb: "You're an individual of formidable strength and grit, your unwavering determination and fierce protectiveness make you a true force to be reckoned with. Yet, amidst your gruff exterior lies a heart burdened by past mistakes, reminding us all that even heroes bear scars, and redemption is a journey worth fighting for."
+
+    },
+    {
+        name: "Joyce Byers",
+        image: "https://i.pinimg.com/736x/00/ee/41/00ee41300ad314c47d5dfd691d1cad01.jpg",
+        blurb: "You're a steadfast guardian fueled by boundless love! Your fierce devotion to your family and unyielding determination in the face of the unknown inspire admiration and awe. Though, within the depths of your soul, echoes the haunting melody of grief and loss, a reminder that even amidst the strangest of circumstances, your humanity remains your greatest strength."
+    },
+    {
+        name: "Mr. Clarke",
+        image: "https://static.wikia.nocookie.net/strangerthings8338/images/1/1a/Scott_Clarke_S2.png",
+        blurb: "You are a wealth of knowledge and enthusiasm! Your passion for science and genuine care for your students guides the path of discovery in Hawkins. Yet, within the realm of equations and experiments, lies the humble reminder that even the brightest minds are fueled by curiosity and empathy, making you an irreplaceable mentor in the journey of learning and growth."
+
+    },
+    {
+        name: 'Martin Brenner a.k.a Papa',
+        image: "https://static.wikia.nocookie.net/strangerthings8338/images/b/be/Brenner_S1.png",
+        blurb: "You're a shadowy figure cloaked in mystery and malice. Your presence looms over the enigmatic events of Hawkins, casting a chilling shroud upon all who dare to cross your path. Yet, amidst the darkness of your deeds, whispers of humanity's capacity for cruelty and manipulation echo, reminding us of the fragile balance between power and corruption in the tangled web of intrigue."
+    },
+    {
+        name: "Bob Newby",
+        image: "https://hips.hearstapps.com/hmg-prod/images/hbz-bob-stranger-things-1509398673.jpg",
+        blurb: "You're a pillar of earnestness and kindness amidst the shadows of Hawkins, your genuine heart and unwavering support bring happiness to those around you. However, within your endearing quirks and unassuming demeanor lies a reminder that even in a world of extraordinary happenings, it's your simple acts of love and courage that define true heroism."
+    },
+    {
+        name: "Dr. Alexi",
+        image: "https://hips.hearstapps.com/hmg-prod/images/mh-mv5bmjllywfkyjutmgi5my00yzg0lwflzjgtzdg4nzuzzgu4nzvkxkeyxkfqcgdeqxvynjuxmjc1otmat-v1-sx1777-cr0-0-1777-896-al-1562560563.jpg",
+        blurb: "You're a figure shrouded in intrigue and mystery, your expertise and knowledge hold the key to unlocking the enigmatic secrets of the Upside Down. Yet, within the depths of your guarded demeanor lies the complexity of a man torn between loyalty and survival, a testament to the intricate layers of humanity woven into the fabric of the unforgving world."
+    },
+    {
+        name: "Murray Bauman",
+        image: "https://i.pinimg.com/originals/9f/4b/6a/9f4b6a1c39bcacb20c4122f13aadd645.jpg",
+        blurb: "You're a maverick investigator unearthing truths in the shadows of Hawkins, your unorthodox methods and sharp wit pierce through the veil of secrecy. Though, beneath the surface of your cynical exterior lies a heart softened by empathy, reminding us that amidst the chaos, genuine connection and understanding are the most powerful tools of all."
+    },
+    {
+        name: "Billy Hangrove",
+        image: "https://static.wikia.nocookie.net/strangerthings8338/images/6/65/Billy_Hargrove.png",
+        blurb: "You're a tempest of contradictions swirling amidst the turmoil of Hawkins, your magnetic charisma and fierce determination leave an indelible mark on those around you. Yet, beneath the facade of bravado and aggression lies the tumultuous struggle of a soul haunted by demons both real and imagined, a poignant reminder of the fragile balance between strength and vulnerability in the tumultuous world of Stranger Things."
+    },
+    {
+        name: "Robin Buckley",
+        image: "https://static.wikia.nocookie.net/strangerthings8338/images/6/65/Robin_Buckley_-_S3_2.jpg",
+        blurb: "You're a lighthouse amidst the stormy seas of Hawkins! Your sharp wit and unwavering resolve guide others through the murky waters of mystery. Beneath your exterior of nonchalance lies a heart brimming with empathy and courage! Reminding us that even in the face of uncertainty, the bonds of friendship can conquer all hardships!."
+    },
+    {
+        name: "Barbara",
+        image: "https://nypost.com/wp-content/uploads/sites/2/2018/09/barb.jpg?quality=75&strip=all",
+        blurb: "You are a gentle soul who departed this world too soon, your kindness and loyalty continue to echo through the corridors of Hawkins. Though your light was extinguished prematurely, your memory serves as a reminder of the fragility of life and the enduring impact of compassion in the hearts of those you left behind."
+    },
+    {
+        name: "The Demogorgon",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4Gb3wdkwBJ_VdLEm_jDSOJ6cXmMT5NiPidNLSZ9ukxw&s",
+        blurb:"You're a primal force unleashed from the depths of the Upside Down. Your monstrous form strikes fear into the hearts of all who dare to encounter you. With razor-sharp claws and a voracious hunger, you prowls the shadows, a relentless predator hunting its prey in the darkest recesses of Hawkins."
+    }
+ ]
